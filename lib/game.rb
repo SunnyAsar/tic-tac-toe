93c5a65773225cc4bcad05cb14e3f471a5cfdadf
Sunny <ring2rings93@gmail.com>
2019-04-03 21:46:21 +0100

    refactor player and game

--- lib/game.rb
+++ lib/game.rb
@@ -2,11 +2,10 @@
 require 'tty-prompt'
 require_relative './player.rb'
 require_relative './board.rb'
-
+# game class
 class Game
-
   def initialize
-    @players = []
+    @board = Board.new
   end
 
   def start
@@ -14,18 +13,23 @@ class Game
     system 'clear'
     puts 'Welcome to tic tac toe'
     ask_players_data
-    board = Board.new
-    board.present_board
+    @board.present_board
   end
 
   def ask_players_data
+    players = []
     2.times do |n|
       player_name = @prompt.ask("What's your name player#{n + 1}?", default: ENV['USER'])
       board_piece = n == 1 ? 'X' : 'O'
-      player = Player.new(player_name, board_piece)
-      @players << player
+      player = Player.new(player_name, board_piece,@board)
+      players << player
     end
-    turn = @prompt.select('Who wants to go first?', @players.map(&:name))
-    @current_player = @players.select { |p| p.name == turn }
+    turn = @prompt.select('Who wants to go first?', players.map(&:name))
+    @current_player, @next_player = players
+    change_player if @next_player.name == turn
+  end
+
+  def change_player
+    @current_player, @next_player = @next_player, @current_player
   end
 end
--- lib/player.rb
+++ lib/player.rb
@@ -1,8 +1,9 @@
 # player class
 class Player
   attr_accessor :name, :board_piece
-  def initialize(name, board_piece)
+  def initialize(name, board_piece, board)
     @name = name
     @board_piece = board_piece
+    @board = board
   end
 end
