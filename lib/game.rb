8e1120088e3a1986405908e56e31ffa8ca8a1811
Sunny <ring2rings93@gmail.com>
2019-04-03 18:45:21 +0100

    added player class

---
+++ lib/game.rb
@@ -0,0 +1,9 @@
+class Game 
+def initialize
+    
+
+end
+
+
+
+end
---
+++ lib/player.rb
@@ -0,0 +1,7 @@
+# player class
+class Player
+  def initialize(name, board_piece)
+    @name = name
+    @board_piece = board_piece
+  end
+end
